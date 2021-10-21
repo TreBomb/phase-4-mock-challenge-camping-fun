010b9011f5e33482632d2b3b4727f595af833870
Trestin <trestinishak@gmail.com>
2021-10-21 14:55:23 -0500

    finished

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,2 +1,31 @@
 class CampersController < ApplicationController
+    def index
+        render json: Camper.all
+    end
+
+    def show
+        camper = Camper.find_by(id: params[:id])
+
+        if camper
+            render json: camper, serializer: CamperWithActivitiesSerializer
+        else
+            render json: {"error": "Camper not found"}, status: :not_found
+        end
+    end
+
+    def create
+        camper = Camper.new(camper_params)
+
+        if camper.save
+            render json: camper, status: :created
+        else
+            render json: {"errors": camper.errors.full_messages}, status: 422
+        end
+    end
+
+    private
+
+    def camper_params
+        params.permit(:name, :age)
+    end
 end
--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,18 @@
 class SignupsController < ApplicationController
+    def create
+        signup = Signup.new(signup_params)
+        activity = Activity.find_by(id: params[:activity_id])
+
+        if signup.save
+            render json: activity, status: :created
+        else
+            render json: {"errors": signup.errors.full_messages}, status: 422
+        end
+    end
+
+    private
+
+    def signup_params
+        params.permit(:time, :camper_id, :activity_id)
+    end
 end
